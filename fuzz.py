e0b7c15dbcfb5b11ef6788aa23ea9bee626adfc0
k45ha1 <matticatee@protonmail.com>
2021-11-28 19:19:32 +0000

    second

---
+++ fuzz.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env	python3
+import glob, subprocess, random, time, threading, os, hashlib, multithreading
+
+# Run one fuzz case with provided input (which is a  byte array)
+def fuzz(thr_id: int, inp: bytearray):
+	assert isinstance(thr_id, int)
+	assert isinstance(inp, bytearray)
+
+	# Write out the input to a temporary file
+	tmpfn = f"tmpinput{thr_id}"
+	with open(tmpfn, "wb") as fd:
+		fd.write(inp)
+
+	# Run objdump until completion
+	sp = subprocess.Popen("./objdump", "-x", tmpfn)
+		stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+	ret = sp.wait()
+
+	# assert that the program ran successfully
+	if ret != 0:
+		print(f"Exited with {ret}")
+
+		if ret == -11:
+			# SIGSEGV
+			dahash = hashlib.sha256(inp).hexdigest()
+			open(os.path.join("crashes", f"crash_{dahash:64}"), "wb").write(inp)
+
+# Get a listing of all the files in the corpus
+# The corpus is the set of files which we pre-seeded the fuzzers with to
+# give it valid inputs.These are files that the program should be able to handle
+# parsing, that we will ultimately mutate and splice together to try to find
+# bugs!
+corpus_files = glob.glob("corpus/*")
+
+# Load the corpus files into memory
+corpus = set()
+
+for filename in corpus_filenames:
+	corpus.add(open(filename, "rb").read()
+
+# Convert the corpus back into a list as we're done with the set for deduping
+# inputs which were not unique
+corpus = list(map(bytearray, corpus))
+
+# Get the time at the start of the fuzzer
+start = time.time()
